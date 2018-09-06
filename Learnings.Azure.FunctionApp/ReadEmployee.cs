31516d86c16fcfb52ce2eb01ec5937cff9cee9db
Rishikesh Jadhav <rishijadhav2792@gmail.com>
2018-09-06 16:41:50 +0530

    Added updates

--- Learnings.Azure.FunctionApp/ReadEmployee.cs
+++ Learnings.Azure.FunctionApp/ReadEmployee.cs
@@ -22,7 +22,7 @@ namespace Learnings.Azure.FunctionApp
                 log.Info("Employee Code: " + messageBody.EmpCode);
                 log.Info("Employee Name: " + messageBody.EmpName);
                 log.Info("Employee Department: " + messageBody.EmpDepartment);
-                log.Info("Processing new message in "+ filter + " subscription - Completed");
+                log.Info("Processing new message in " + filter + " subscription - Completed");
                 log.Info("----------------------------------------------------------------------------------------------------------------------------");
             }
             catch (Exception exception)
@@ -32,24 +32,28 @@ namespace Learnings.Azure.FunctionApp
             }
         }
 
+        //[Disable]
         [FunctionName("ReadHREmployee")]
         public static void ReadHREmployee([ServiceBusTrigger("%Topic%", "%HRSubscription%", AccessRights.Listen, Connection = "AzureWebJobsServiceBus")]BrokeredMessage message, TraceWriter log)
         {
             ProcessEmployee(message, log, "HR");
         }
 
+        //[Disable]
         [FunctionName("ReadAdminEmployee")]
         public static void ReadAdminEmployee([ServiceBusTrigger("%Topic%", "%AdminSubscription%", AccessRights.Listen, Connection = "AzureWebJobsServiceBus")]BrokeredMessage message, TraceWriter log)
         {
             ProcessEmployee(message, log, "Admin");
         }
 
+        //[Disable]
         [FunctionName("ReadITEmployee")]
         public static void ReadITEmployee([ServiceBusTrigger("%Topic%", "%ITSubscription%", AccessRights.Listen, Connection = "AzureWebJobsServiceBus")]BrokeredMessage message, TraceWriter log)
         {
             ProcessEmployee(message, log, "IT");
         }
 
+        [Disable]
         [FunctionName("ReadAllEmployee")]
         public static void ReadAllEmployee([ServiceBusTrigger("%Topic%", "%AllSubscription%", AccessRights.Listen, Connection = "AzureWebJobsServiceBus")]BrokeredMessage message, TraceWriter log)
         {
---
+++ Learnings.Azure.WebJob/Functions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+
+namespace Learnings.Azure.WebJob
+{
+    public class Functions
+    {
+        // This function will get triggered/executed when a new message is written 
+        // on an Azure Queue called queue.
+        public static void ProcessQueueMessage([QueueTrigger("queue11")] string message, TextWriter log)
+        {
+            log.WriteLine(message);
+            Console.WriteLine("New message: " + message);
+        }
+    }
+}
